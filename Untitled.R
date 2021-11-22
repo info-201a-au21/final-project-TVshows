60b84a3ca925da837385673460d5b07a6f29c6ab
whoisyeslek <jiawenli@uw.edu>
2021-11-21 23:32:56 -0800

    new file

---
+++ Untitled.R
@@ -0,0 +1,11 @@
+# Load datasets
+TV_show_4 <- read.csv("tv_shows.csv")
+
+# Which platform includes the largest amount of TV shows? 
+# Which platform is the most recommended for a family, which have children to subscribe? 
+bar plot
+# Which platform has the most enormous amount of TV shows (in a year)?
+# Which platform includes largest amount of TV shows with IMDb >  9.0?
+# How does the average IMDb rate changed over N year? line/ scatterplot
+# Which platform has most TV show original?
+# Top used language? 
