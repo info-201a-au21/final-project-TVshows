c092a624ae2646f74d6c86acc8345d07ab41c556
your-full-name <bibi@zhangyixingnvyoudeMacBook-Pro.local>
2021-11-22 23:44:56 -0800

    Scarlet

--- table.R
+++ table.R
@@ -2,5 +2,6 @@ library(knitr)
 library(kableExtra)
 
 TV_show_4 %>% select(Title, Year, Age, IMDb, Netflix, Hulu, Prime.Video, Disney.) %>%
+  group_by(Year) %>%
   kable(caption = 'Table: TV Shows (Netflix, Hulu, Prime, Disney)') %>%
   kable_styling(bootstrap_options = c('striped', 'hover', 'responsive', 'condensed'))
