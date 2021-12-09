cb8c4f56c7cef0790e0550debe154fdf2701749a
lhlorlhr <thunder@Helens-MacBook-Pro.local>
2021-12-08 21:00:36 -0800

    threefile

---
+++ app.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+source("app_ui.R")
+source("app_server.R")
+shinyApp(ui = ui, server = server)
---
+++ app_server.R
@@ -0,0 +1,8 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+
+server <- function(input, output) {
+  
+}
