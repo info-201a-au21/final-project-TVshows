699eb1539569150a30b100a000907916b64df94d
your-full-name <bibi@zhangyixingnvyoudeMacBook-Pro.local>
2021-12-09 11:58:24 -0800

    szzz

--- app_ui.R
+++ app_ui.R
@@ -68,9 +68,11 @@ line_panel <- tabPanel(
 intro_tab <- tabPanel(
   "Introduction",
   titlePanel("Introduction"), 
-  fluidPage(
-    img("", src = "fp1.jpg"),
     h3("Team Member: Helen Lei, Kelsey Li, Scarlet Zheng"),
+  fluidPage(
+    img("", src = "https://editor.analyticsvidhya.com/uploads/58713tvshows1.jpeg",
+        height='340px',width='500px'),
+    
     br(),
     h4("Background"),
     p("We are interested in the quality and popularity of TV shows in the 
@@ -86,7 +88,10 @@ intro_tab <- tabPanel(
      helps us a lot since she puts information in one CSV file. Neelima Jauhari 
      collects the other one, which is the Amazon Prime website. This one focus 
      on one platform, and we explore the diversity of languages in our 
-     analysis.")
+     analysis."),
+    br(),
+    img("", src = "https://img.wattpad.com/883b5477c7bfcec4cb342f35a8c42e4b6e2c196c/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f56475136637557683750675756773d3d2d3239372e313464643463303262383265643764323435383934363733393836322e6a7067",
+        height='500px',width='492px')
   )
   
 )
@@ -121,7 +126,10 @@ conclude_tab <- tabPanel(
       of the features of humanity. The difference between languages shows the 
       various ways that people communicate with each other. People can 
       communicate by using their languages, so the diversity of languages 
-      in a platform builds a bridge for globalization.")
+      in a platform builds a bridge for globalization."),
+    br(),
+    img("", src = "https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/125433906_2062027213932735_4872550311353998669_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_F8xsBW9eMEAX-Lt2gD&_nc_oc=AQm1wJlERariU2ZUOz2FJZNkAIYUDiO6eNxeRmIChikwa3eklQ5qHG2BlihkGY4D6TJ6FVKdMDJhexuHHtOIyn18&_nc_ht=scontent-sea1-1.xx&oh=a2f127b1d47bd9acb7ddd29e9096aa48&oe=61D699CD",
+          height='400px',width='400px')
   )
 )
 
