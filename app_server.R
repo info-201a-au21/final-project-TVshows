ed6ac009a41d10f35c3755b0901c9a5a7d2afd6a
lhlorlhr <thunder@Helens-MacBook-Pro.local>
2021-12-09 12:03:47 -0800

    barchart

--- app_server.R
+++ app_server.R
@@ -3,15 +3,156 @@ library(shiny)
 library(plotly)
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
 library(base)
 library(ggrepel)
+
+
+# Load datasets
+TV_show_4 <- read.csv("tv_shows.csv")
+
+
+#1. Which platform includes the largest amount of TV shows? 
+#Netflix
+(amount_Netflix <- TV_show_4 %>%
+    filter(Netflix == 1) %>%
+    summarise(sum_Netflix = sum(Netflix)))
+
+#Hulu
+(amount_Hulu <- TV_show_4 %>%
+    filter(Hulu == 1) %>%
+    summarise(sum_Hulu = sum(Hulu)))
+
+#Prime.Video
+(amount_Prime.Video <- TV_show_4 %>%
+    filter(Prime.Video == 1) %>%
+    summarise(sum_Prime.Video = sum(Prime.Video)))
+
+#Disney.
+(amount_Disney. <- TV_show_4 %>%
+    filter(Disney. == 1) %>%
+    summarise(sum_Disney. = sum(Disney.)))
+
+amount_each_platform <- cbind(amount_Disney., amount_Hulu, amount_Netflix, amount_Prime.Video)
+amount_each_platform$Max <- max(amount_each_platform[1:4])
+
+# 2. Which platform is the most recommended for a family, which have children to subscribe? 
+## bar plot (blank in Age didn't remove yet)
+(netflix_underage <- TV_show_4 %>%
+    filter(Age != "18+", na.rm = TRUE) %>%
+    filter(Age != "all", na.rm = TRUE) %>%
+    filter(Netflix == 1, na.rm = TRUE) %>%
+    nrow()
+)
+(hulu_underage <- TV_show_4 %>%
+    filter(Age != "18+", na.rm = TRUE) %>%
+    filter(Age != "all", na.rm = TRUE) %>%
+    filter(Hulu == 1, na.rm = TRUE) %>%
+    nrow()
+)
+(prime_underage <- TV_show_4 %>%
+    filter(Age != "18+", na.rm = TRUE) %>%
+    filter(Age != "all", na.rm = TRUE) %>%
+    filter(Prime.Video == 1, na.rm = TRUE) %>%
+    nrow())
+
+(disney_underage <- TV_show_4 %>%
+    filter(Age != "18+", na.rm = TRUE) %>%
+    filter(Age != "all", na.rm = TRUE) %>%
+    filter(Disney. == 1, na.rm = TRUE) %>%
+    nrow())
+
+all_underage <- rbind(netflix_underage, hulu_underage, prime_underage, disney_underage)
+all_underage_name <- c("Netflix", "Hulu", "Prime Video", "Disney")
+colnames(all_underage) <- "value"
+
+
+# 9.0+
+TV_show_4$IMDb <- sub("/10", "", TV_show_4$IMDb)
+IMDb_Netflix_9.0 <- TV_show_4 %>%
+  filter(IMDb > 9.0, na.rm = TRUE,
+         Netflix == 1) %>%
+  summarise(IMDb_Netflix_9.0 = sum(Netflix, na.rm = TRUE))
+
+IMDb_Hulu_9.0 <- TV_show_4 %>%
+  filter(IMDb > 9.0, na.rm = TRUE,
+         Hulu == 1) %>%
+  summarise(IMDb_Hulu_9.0 = sum(Hulu, na.rm = TRUE))
+
+IMDb_Prime_9.0 <- TV_show_4 %>%
+  filter(IMDb > 9.0, na.rm = TRUE,
+         Prime.Video == 1) %>%
+  summarise(IMDb_Prime_9.0 = sum(Prime.Video, na.rm = TRUE))
+
+IMDb_Disney_9.0 <- TV_show_4 %>%
+  filter(IMDb > 9.0, na.rm = TRUE,
+         Disney. == 1) %>%
+  summarise(IMDb_Disney_9.0 = sum(Disney., na.rm = TRUE))
+
+# 7.0-9.0
+IMDb_Netflix_8.0 <- TV_show_4 %>%
+  filter(IMDb > 7.0, IMDb <= 9.0, na.rm = TRUE,
+         Netflix == 1) %>%
+  summarise(IMDb_Netflix_8.0 = sum(Netflix, na.rm = TRUE))
+
+IMDb_Hulu_8.0 <- TV_show_4 %>%
+  filter(IMDb > 7.0, IMDb <= 9.0, na.rm = TRUE,
+         Hulu == 1) %>%
+  summarise(IMDb_Hulu_8.0 = sum(Hulu, na.rm = TRUE))
+
+IMDb_Prime_8.0 <- TV_show_4 %>%
+  filter(IMDb > 7.0, IMDb <= 9.0, na.rm = TRUE,
+         Prime.Video == 1) %>%
+  summarise(IMDb_Prime_8.0 = sum(Prime.Video, na.rm = TRUE))
+
+IMDb_Disney_8.0 <- TV_show_4 %>%
+  filter(IMDb > 7.0, IMDb <= 9.0, na.rm = TRUE,
+         Disney. == 1) %>%
+  summarise(IMDb_Disney_8.0 = sum(Disney., na.rm = TRUE))
+
+# 7.0 - 
+IMDb_Netflix_7.0 <- TV_show_4 %>%
+  filter(IMDb < 7.0, na.rm = TRUE,
+         Netflix == 1) %>%
+  summarise(IMDb_Netflix_7.0 = sum(Netflix, na.rm = TRUE))
+
+IMDb_Hulu_7.0 <- TV_show_4 %>%
+  filter(IMDb < 7.0, na.rm = TRUE,
+         Hulu == 1) %>%
+  summarise(IMDb_Hulu_7.0 = sum(Hulu, na.rm = TRUE))
+
+IMDb_Prime_7.0 <- TV_show_4 %>%
+  filter(IMDb < 7.0, na.rm = TRUE,
+         Prime.Video == 1) %>%
+  summarise(IMDb_Prime_7.0 = sum(Prime.Video, na.rm = TRUE))
+
+IMDb_Disney_7.0 <- TV_show_4 %>%
+  filter(IMDb < 7.0, na.rm = TRUE,
+         Disney. == 1) %>%
+  summarise(IMDb_Disney_7.0 = sum(Disney., na.rm = TRUE))
+
+
+overview_IMDb_9.0 <- cbind(IMDb_Netflix_9.0, IMDb_Hulu_9.0, IMDb_Disney_9.0, IMDb_Prime_9.0)
+overview_IMDb_8.0 <- cbind(IMDb_Netflix_8.0, IMDb_Hulu_8.0, IMDb_Disney_8.0, IMDb_Prime_8.0)
+overview_IMDb_7.0 <- cbind(IMDb_Netflix_7.0, IMDb_Hulu_7.0, IMDb_Disney_7.0, IMDb_Prime_7.0)
+new_IMDb_9.0 <- as.data.frame(t(overview_IMDb_9.0))
+new_IMDb_8.0 <- as.data.frame(t(overview_IMDb_8.0))
+new_IMDb_7.0 <- as.data.frame(t(overview_IMDb_7.0))
+# colnames(new_IMDb_9.0) <- c("< 0.7", "0.7 - 0.9", "0.9+")
+# colnames(new_IMDb_8.0) <- c("< 0.7", "0.7 - 0.9", "0.9+")
+# colnames(new_IMDb_7.0) <- c("< 0.7", "0.7 - 0.9", "0.9+")
+new_IMDb_all <- cbind(new_IMDb_7.0, new_IMDb_8.0, new_IMDb_9.0)
+colnames(new_IMDb_all) <- c("< 0.7", "0.7 - 0.9", "0.9+")
+rownames(new_IMDb_all) <- c("Netflix", "Hulu", "Disney+", "Prime")
+x_axis <- c("Netflix", "Hulu", "Disney+", "Prime")
+
+
+
 library(tidyverse)
 library(scales)
 library(RColorBrewer)
 
-
 ## read data
-# pie chart
 Prime_data <- read.csv("Prime TV Shows Data set.csv", stringsAsFactors = F)
 prime_lang <- Prime_data %>%
   group_by(Language) %>%
@@ -23,68 +164,8 @@ prime_lang <- Prime_data %>%
   filter(lang_prop > 0.005) # discard proportion that is less then 0.5
 prime_lang$lang_prop <- percent(prime_lang$lang_prop, accuracy = 0.1)
 
-# bar chart
-TV_show_4 <- read.csv("tv_shows.csv")
-TV_show_4$IMDb <- sub("/10", "", TV_show_4$IMDb)
-IMDb_Netflix_9.0 <- TV_show_4 %>%
-  filter(IMDb > 9.0, na.rm = TRUE,
-         Netflix == 1) %>%
-  summarise(IMDb_Netflix = sum(Netflix, na.rm = TRUE))
-
-IMDb_Hulu_9.0 <- TV_show_4 %>%
-  filter(IMDb > 9.0, na.rm = TRUE,
-         Hulu == 1) %>%
-  summarise(IMDb_Hulu = sum(Hulu, na.rm = TRUE))
-
-IMDb_Prime_9.0 <- TV_show_4 %>%
-  filter(IMDb > 9.0, na.rm = TRUE,
-         Prime.Video == 1) %>%
-  summarise(IMDb_Prime = sum(Prime.Video, na.rm = TRUE))
-
-IMDb_Disney_9.0 <- TV_show_4 %>%
-  filter(IMDb > 9.0, na.rm = TRUE,
-         Disney. == 1) %>%
-  summarise(IMDb_Disney = sum(Disney., na.rm = TRUE))
-
-overview_IMDb <- cbind(IMDb_Netflix_9.0, IMDb_Hulu_9.0, IMDb_Disney_9.0, IMDb_Prime_9.0)
-new_IMDb <- t(overview_IMDb)
-new_IMDb <- data.frame(names = row.names(new_IMDb), new_IMDb)
-
-# line chart
-(year_Neflix <- TV_show_4 %>%
-    group_by(Year) %>%
-    filter(Netflix == 1) %>%
-    summarise(netflix_per_year = sum(Netflix))
-)
-
-(year_Hulu <- TV_show_4 %>%
-    group_by(Year) %>%
-    filter(Hulu == 1) %>%
-    summarise(hulu_per_year = sum(Hulu)))
-
-year_Prime <- TV_show_4 %>%
-  group_by(Year) %>%
-  filter(Prime.Video == 1) %>%
-  summarise(prime_per_year = sum(Prime.Video))
-
-year_Disney <- TV_show_4 %>%
-  group_by(Year) %>%
-  filter(Disney. == 1) %>%
-  summarise(disney_per_year = sum(Disney.))
-
-year_aggregate <- year_Neflix %>%
-  full_join(year_Hulu, by = "Year") %>%
-  full_join(year_Prime, by = "Year") %>%
-  full_join(year_Disney, by = "Year") %>%
-  filter(str_length(Year) == 4)
-
-year_aggregate[is.na.data.frame(year_aggregate)] <- 0
-
-color <- c("netflix" = "red", "hulu" = "green", "prime" = "blue", "disney" = "purple")
-
 ## sever
 server <- function(input, output) { 
-  # pie chart
   output$pie <- renderPlot({
     lang_pie <- ggplot(prime_lang, aes(x = "", y = lang_prop, fill = Language)) +
       geom_bar(stat = "identity") +
@@ -96,42 +177,23 @@ server <- function(input, output) {
         labs(title = "Language Proportions", y = "", x = "") +
         scale_fill_brewer(palette="Paired")
     }
-    
-    lang_pie 
+    return(lang_pie) 
   })
   
-  # bar chart
   output$bar <- renderPlot({
-    if (input$pie) {
-      bar_IMDb <- ggplot(new_IMDb, aes(x = "", y = new_IMDb, fill = names)) +
-        geom_bar(stat = "identity") +
-        labs(title = "IMDb Higher Than 9.0", y = "", x = "") +
-        coord_polar("y", start = 0) +
-        geom_text(aes(label = paste0(new_IMDb)), position = position_stack(vjust=0.5)) +
-        labs(title = "IMDb Higher Than 9.0", y = "", x = "") +
-        scale_fill_brewer(palette="Paired")
-    } else {
-      bar_IMDb <- ggplot(data = new_IMDb, mapping = aes(x = names,
-                                                        y = new_IMDb,
-                                                        fill = names)) +
-        geom_bar(stat = "identity") +
-        labs(x = "Platforms", y = "Amount of TV shows with 9.0 +", title = "IMDb Higher Than 9.0")
-    }
-    bar_IMDb
+    bar_IMDb <- ggplot(data = new_IMDb_all) +
+      geom_bar(aes(x = x_axis,
+                   y = !!as.name(input$range),
+                   fill = x_axis),
+               stat = "identity") +
+      labs(x = "Platforms", 
+           y = "Amount of TV shows with 9.0 +", 
+           title = "IMDb Distribution")
+    #bar_plotly <- ggplotly(bar_IMDb)
+    return(bar_IMDb)
   })
   
-  # line chart
-  output$line <- renderPlot({
-    
-    plot_data <- year_aggregate %>%
-      filter(Year > input$year[1], Year < input$year[2])
-    
-    ggplot(data = plot_data, aes(x = "Year")) +
-      geom_line(aes(y = netflix_per_year, color = "netflix")) +
-      geom_line(aes(y = hulu_per_year, color = "hulu")) + 
-      geom_line(aes(y = prime_per_year, color = "prime")) +
-      geom_line(aes(y = disney_per_year, color = "disney")) +
-      labs(x = input$year, y = "Number of TV Shows", title = "The Amount of TV Shows on 4 Platforms Each Year") +
-      scale_color_manual(values = color)
-  })
+  output$
+  
+ 
 }
--- app_ui.R
+++ app_ui.R
@@ -2,18 +2,76 @@
 library(shiny)
 library(plotly)
 
-## Chart
-#pie
+# Linear Plot
+linear_chart_sidebar <- sidebarPanel(
+  sliderInput(
+    inputId = "linear",
+    label = "Year Range",
+    min = 1900,
+    max = 2025,
+    value = c(1925,2000)
+  )
+)
+
+linear_chart_main <- mainPanel(
+  plotlyOutput(outputId = "linear")
+)
+
+linear_chart_tab <- tabPanel(
+  "Linear Chart Visulization",
+  titlePanel("Linear chart"),
+  sidebarLayout(
+    linear_chart_sidebar,
+    linear_chart_main,
+  ),
+  fluidPage(
+    p("some words")
+  )
+)
+
+# Bar Chart
+bar_chart_sidebar <- sidebarPanel(
+  selectInput(
+    inputId = "range",
+    label = "IMDb Rating Range",
+    choices = list(
+      "IMDb > 9.0" = "0.9+",
+      "7.0 < IMDb <= 9.0" = "0.7 - 0.9",
+      "IMDb < 7.0" = "< 0.7"
+    ),
+    selected = "IMDb > 9.0"
+  )
+)
+
+bar_chart_main <- mainPanel(
+  plotlyOutput(outputId = "bar")
+)
+
+bar_chart_tab <- tabPanel(
+  "Bar Chart Visulization",
+  titlePanel("Bar chart"),
+  sidebarLayout(
+    bar_chart_sidebar,
+    bar_chart_main,
+  ),
+  fluidPage(
+    p("some words")
+  )
+)
+
+# Pie Chart
 pie_sidebar_content <- sidebarPanel(
-  checkboxInput("data", label = strong("show data"), value = TRUE)
+  checkboxInput(inputId = "data", 
+                label = strong("show data and change color"), 
+                value = TRUE)
 )
 
 pie_main_content <- mainPanel(
-  plotOutput("pie")
+  plotOutput(outputId = "pie")
 )
 
-pie_panel <- tabPanel(
-  "Pie chart",
+pie_chart_tab <- tabPanel(
+  "Pie Chart Visualization",
   titlePanel("Pie chart"), 
   sidebarLayout(
     pie_sidebar_content,
@@ -21,77 +79,13 @@ pie_panel <- tabPanel(
   )
 )
 
-#bar chart
-bar_sidebar_content <- sidebarPanel(
-  checkboxInput("pie", label = strong("Turn into pie chart"), value = F)
-)
-
-bar_main_content <- mainPanel(
-  plotOutput("bar")
-)
-
-bar_panel <- tabPanel(
-  "Bar chart",
-  titlePanel("Bar chart"), 
-  sidebarLayout(
-    bar_sidebar_content,
-    bar_main_content
-  )
-)
-
-#line chart
-year_range <- as.numeric(range(year_aggregate$Year)) 
-line_sidebar_content <- sidebarPanel(
-  sliderInput(
-    "year",
-    label = "Year", 
-    min = year_range[1],
-    max = year_range[2], 
-    value = c(2000,2020)
-  )
-)
-
-line_main_content <- mainPanel(
-  plotOutput("line")
-)
-
-line_panel <- tabPanel(
-  "Line chart",
-  titlePanel("Line chart"), 
-  sidebarLayout(
-    line_sidebar_content,
-    line_main_content
-  )
-)
 
 # Introductory
 intro_tab <- tabPanel(
-  "Introduction",
-  titlePanel("Introduction"), 
-    h3("Team Member: Helen Lei, Kelsey Li, Scarlet Zheng"),
+  "Intro",
+  tags$img(src = "./Netflix-Amazon-Prime-Hulu-Disney-Plus-Logos.png"),
   fluidPage(
-    img("", src = "https://editor.analyticsvidhya.com/uploads/58713tvshows1.jpeg",
-        height='340px',width='500px'),
-    
-    br(),
-    h4("Background"),
-    p("We are interested in the quality and popularity of TV shows in the 
-     mainstreaming platforms, including Netflix, Prime Video, Hulu, and Disney+. 
-     During the pandemic period, people stay at home and do limited activity. A 
-     tremendous amount of people tend to watch TV shows during quarantine which 
-     lead streaming platforms to increas subscribers."),
-    br(),
-    h4("Database"),
-    p("First of all, we use the data set collected by Ruchi Bhatia, and the 
-     methodology that she uses is IMDb (data was scraped using Beautiful Soup). 
-     This intergraded data set contains the data from several platforms, which 
-     helps us a lot since she puts information in one CSV file. Neelima Jauhari 
-     collects the other one, which is the Amazon Prime website. This one focus 
-     on one platform, and we explore the diversity of languages in our 
-     analysis."),
-    br(),
-    img("", src = "https://img.wattpad.com/883b5477c7bfcec4cb342f35a8c42e4b6e2c196c/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f56475136637557683750675756773d3d2d3239372e313464643463303262383265643764323435383934363733393836322e6a7067",
-        height='500px',width='492px')
+    p(paste("blah blah blah"))
   )
   
 )
@@ -99,37 +93,9 @@ intro_tab <- tabPanel(
 # Conclusion
 conclude_tab <- tabPanel(
   "Conclusion",
-  titlePanel("Conclusion"), 
   fluidPage(
-    h4("1. Blooming of Streaming Services"),
-    p("Since the year of 2000, streaming services have started blooming. 
-      As two market leaders, Netflix and Amazon Prime Video, brought accessibility 
-      to new TV shows to their peak within the past 20 years, TV shows production 
-      was stimulated in return. However, the pandemic in 2020 hindered the productions,
-      and a massive reduction in numbers was presented."),
-    br(),
-    h4("2. Trend of TVshows"),
-    p("In this big data era, internet databases collect reviews and ratings of movies and tv 
-       series is a phenomenon and provide a reference for users to make tv show selections. 
-       Within the 10 point rating scale, subscribers are able to pick high-quality shows easily. 
-       From the dataset, the most recommended platforms for including a larger number of highly-rated 
-       tv-shows are Netflix and Hulu."),
-    br(),
-    h4("3. The Diversity of Language"),
-    p("Many TV shows have different languages applied on the Amazon Prime 
-      platform. The diversity of languages reflects the diversity in the US. 
-      In one perspective, as a platform that has the most subscription from US 
-      citizens, Amazon Prime explore the market of other languages such as 
-      Hindi, Japanese, etc., and it attracts customers who can speak other 
-      languages to subscribe. For example, Japanese-Americans would like to 
-      subscribe because of Japanese TV shows. In otherwise, languages are one 
-      of the features of humanity. The difference between languages shows the 
-      various ways that people communicate with each other. People can 
-      communicate by using their languages, so the diversity of languages 
-      in a platform builds a bridge for globalization."),
-    br(),
-    img("", src = "https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/125433906_2062027213932735_4872550311353998669_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=_F8xsBW9eMEAX-Lt2gD&_nc_oc=AQm1wJlERariU2ZUOz2FJZNkAIYUDiO6eNxeRmIChikwa3eklQ5qHG2BlihkGY4D6TJ6FVKdMDJhexuHHtOIyn18&_nc_ht=scontent-sea1-1.xx&oh=a2f127b1d47bd9acb7ddd29e9096aa48&oe=61D699CD",
-          height='400px',width='400px')
+    h1("This is a title"),
+    p(paste("blah blah blah"))
   )
 )
 
@@ -137,8 +103,8 @@ conclude_tab <- tabPanel(
 ui <- navbarPage(
   "TV show",
   intro_tab,
-  line_panel,
-  bar_panel,
-  pie_panel,
+  linear_chart_tab,
+  bar_chart_tab,
+  pie_chart_tab,
   conclude_tab
 )
